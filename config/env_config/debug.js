7e53f68e7269b0d64d3c69c7ed8bd46fbb9e77d2
xb_grey <xb_grey@163.com>
2018-04-26 23:26:48 +0800

    环境变量

--- config/env_config/debug.js
+++ config/env_config/debug.js
@@ -1,8 +1,16 @@
-module.exports = require('../../evn') || {
-    NODE_ENV:'debug',
-    IS_MOCK:true,//是否开启mock数据
-    USERNAME:'kxladmin',//默认用户名
-    PASSWORD:'123456',//默认密码
-    API_URI:undefined,//服务器API头
-    DOMAIN:'kxl.com',//根域名
+let evn = {};
+try {
+    evn = require('../../evn');
+} catch (error) {
+    
+}
+
+module.exports = {
+    NODE_ENV: 'debug',
+    IS_MOCK: true,//是否开启mock数据
+    USERNAME: undefined,//默认用户名
+    PASSWORD: undefined,//默认密码
+    API_URI: undefined,//服务器API头
+    DOMAIN: 'kxl.com',//根域名
+    ...evn
 };
