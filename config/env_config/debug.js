a3d9a841ebf99dae9dc28b3020aebe1743580a3a
xb_grey <xb_grey@163.com>
2018-04-27 17:00:12 +0800

    修改了发布脚本

--- config/env_config/debug.js
+++ config/env_config/debug.js
@@ -1,16 +1,20 @@
-let evn = {};
-try {
-    evn = require('../../evn');
-} catch (error) {
-    
-}
-
-module.exports = {
+let evn = {
     NODE_ENV: 'debug',
     IS_MOCK: true,//是否开启mock数据
     USERNAME: undefined,//默认用户名
     PASSWORD: undefined,//默认密码
     API_URI: undefined,//服务器API头
     DOMAIN: 'xltec.cc',//根域名
-    ...evn
 };
+
+try {
+    var info = require('../../evn');
+} catch (error) {
+    var info = {};
+}
+
+for(var k in info){
+    evn[k] = info[k];
+}
+
+module.exports = evn;
