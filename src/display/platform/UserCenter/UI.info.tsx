486fc77601f4dbc14aae33b744b5eb8afc62a635
dutao <1173127007@qq.com>
2018-05-04 10:30:14 +0800

    fix: 格式不规范，代码冗余

--- src/display/platform/UserCenter/UI.info.tsx
+++ src/display/platform/UserCenter/UI.info.tsx
@@ -2,28 +2,22 @@ import * as React from 'react';
 import { UIBasic, IPropsBasic } from 'kts-scaffold-framework/modules';
 import { connect } from 'src/redux';
 import language from 'src/entry/Language';
+import { IUserInfo } from 'src/dataModel';
 import ReduxState, { } from 'src/redux/ReduxState';
 import ModulesState from './Modules.State';
 import ModulesAction from './Modules.Action';
-import { UserType } from 'src/entry/constant';
 import {
     Card,
     Input,
 } from 'antd';
 import DescriptionList  from 'ant-design-pro/lib/DescriptionList';
-import 'ant-design-pro/dist/ant-design-pro.css';
 
 const { Description } = DescriptionList;
 const css = require('./index.scss');
 
 /** Redux接口 */
 interface IReduxStatePart {
-    nickName?: string;
-    userType?: UserType;
-    phone?: string;
-    email?: string;
-    departmentName?: string;
-    description?: string;
+    userInfo?: IUserInfo;
 }
 
 /** Props接口 */
@@ -33,12 +27,7 @@ interface IProps extends IReduxStatePart, IPropsBasic {
 
 /** 绑定全局数据到props */
 @connect((state: ReduxState): IReduxStatePart => ({
-    nickName: state.user.userInfo.nickName,
-    userType: state.user.userInfo.userType,
-    phone: state.user.userInfo.phone,
-    email: state.user.userInfo.email,
-    departmentName: state.user.userInfo.departmentName,
-    description: state.user.userInfo.description,
+    userInfo: state.user.userInfo,
 }))
 export default class UIComponents extends UIBasic<IProps, ModulesState> {
 
@@ -48,31 +37,33 @@ export default class UIComponents extends UIBasic<IProps, ModulesState> {
 
     /** 用户类型名称 */
     private get nickNameName(): string {
-        const userTypeString: string = language.userTypeToString(this.props.userType);
+        const {userInfo} = this.props;
+        const userTypeString: string = language.userTypeToString(userInfo.userType);
         return userTypeString || '未知';
     }
     
     render() {
+        const {userInfo} = this.props;
         return (
             <Card title="用户资料" className={css.info}>
                 <DescriptionList title="" col={2} size="large" gutter={120}> 
                     <Description term="员工姓名">
-                        {this.props.nickName}
+                        {userInfo.nickName}
                     </Description>
                     <Description term="电话">
-                        {this.props.phone}
+                        {userInfo.phone}
                     </Description>
                     <Description term="邮箱">
-                        {this.props.email}
+                        {userInfo.email}
                     </Description>
                     <Description term="所属部门">
-                        {this.props.departmentName}
+                        {userInfo.departmentName}
                     </Description>
                     <Description term="角色">
                         <Input value={this.nickNameName} disabled={true}/>
                     </Description>
                     <Description term="备注信息">
-                        {this.props.description}
+                        {userInfo.description}
                     </Description>
                 </DescriptionList>
             </Card>
