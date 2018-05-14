153bd684bde33a64fcad95088efcc8d55f554af1
xb_grey <xb_grey@163.com>
2018-05-14 22:58:50 +0800

    调整项目

--- src/display/modules/Home/Workbench/Modules.Route.tsx
+++ src/display/modules/Home/Workbench/Modules.Route.tsx
@@ -9,9 +9,9 @@ Route.addNode(
     parentName,
     { nodeName: 'workbench/app', path: '/workbench/app/:appid', component: UIIframe },
     { nodeName: 'workbench/invoiceInput', path: '/workbench/invoiceInput', title: '发票录入' , component: apps.InvoiceInput },
-    { nodeName: '/workbench/platform/userCenter', path: '/workbench/platform/userCenter', title: '用户中心' , component: platform.UserCenter },
-    { nodeName: '/workbench/platform/inBox', path: '/workbench/platform/inBox', title: '收件箱' , component: platform.InBox },
-    { nodeName: '/workbench/platform/systemManage', path: '/workbench/platform/systemManage', title: '系统管理' , component: platform.SystemManage },
+    { nodeName: 'workbench/platform/userCenter', path: '/workbench/platform/userCenter', title: '用户中心' , component: platform.UserCenter },
+    { nodeName: 'workbench/platform/inBox', path: '/workbench/platform/inBox', title: '收件箱' , component: platform.InBox },
+    { nodeName: 'workbench/platform/systemManage', path: '/workbench/platform/systemManage', title: '系统管理' , component: platform.SystemManage },
 );
 
 export default {
