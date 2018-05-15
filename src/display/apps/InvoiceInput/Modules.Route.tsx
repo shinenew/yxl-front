99af995e908d3942d3aaebdc349d8b32a456c729
周小瑜 <zhouxiaoyu@zhouxiaoyudeMacBook-Pro.local>
2018-05-15 16:13:47 +0800

    冲突解决

---
+++ src/display/apps/InvoiceInput/Modules.Route.tsx
@@ -0,0 +1,22 @@
+import { Route } from 'src/routes';
+import UIForm from './TableList';
+import { InvoiceDetails } from 'src/display/part';
+
+const parentName: string = 'invoiceInput'; // 路由的跟节点；
+
+Route.addNode(
+    parentName,
+    { nodeName: 'list',exact:true, path: '*/invoiceInput', component: UIForm },
+    { nodeName: 'group',exact:true, path: '*/group/:id', component: require('./Group').default },
+    { nodeName: 'invoiceDetail', path: 'invoiceInput/invoiceDetail', title: '发票详情' , component: InvoiceDetails},
+);
+
+export default {
+    getChildReact: (index?: string) => {
+        return Route.getChildReact(parentName, index);
+    },
+};
+
+export {
+    Switch
+} from 'react-router-dom';
