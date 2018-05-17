8d26cfa4d62ed50bbc3c99daeb1b324474dfa716
dutao <1173127007@qq.com>
2018-05-17 10:38:44 +0800

    fix: UI微调

--- src/api/invoiceInput/groupInfo/IData.ts
+++ src/api/invoiceInput/groupInfo/IData.ts
@@ -3,7 +3,25 @@
  */
 export default interface IData {
     detailInfoList: {
-
+        groupDetailId?: string;
+        invoiceCode?: string;
+        invoiceGroupId?: string;
+        invoiceGroupNumber?: string;
+        invoiceLoggingId?: number;
+        invoiceNumber?: string;
+        matchMessage?: string;
+        matchState?: number;
+        receivedState?: number;
+        waitState?: number;
     }[];
-    groupInfo: {};
+    groupInfo: {
+        createType?: number;
+        groupId?: string;
+        groupNumber?: string;
+        invoiceCount: number;
+        loggedCount?: number;
+        matchMessage?: string;
+        matchState?: number;
+        waitCount?: number;
+    };
 }
