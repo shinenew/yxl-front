b4990dd33d34403a8e6d6d41dd005d95c51f2f41
邱郝艺 <876434739@qq.com>
2018-05-17 15:52:02 +0800

    修改用户管理

--- src/display/platform/SystemManage/Modules.State.ts
+++ src/display/platform/SystemManage/Modules.State.ts
@@ -1,6 +1,7 @@
 import ModulesStateBasic from 'kts-scaffold-framework/modules/ModulesStateBasic';
 import { MyStore } from 'src/redux';
 import ModulesStateDepartment from './Modules.State.Department';
+import ModulesStateUser from './Modules.State.User';
 
 /** 模块状态 */
 export default class ModulesState extends ModulesStateBasic {
@@ -8,6 +9,9 @@ export default class ModulesState extends ModulesStateBasic {
     public depModulesState = new ModulesStateDepartment();
     /************************** 部门管理end ************************************/
 
+    /** 用户管理 */
+    public userModulesState = new ModulesStateUser();
+    
     /************************** 关联公司及集团管理start ************************************/
     user = MyStore.instance.getState().user.userInfo;
     url = MyStore.instance.getState().user.zoneUrl;
@@ -34,43 +38,6 @@ export default class ModulesState extends ModulesStateBasic {
         createTime: '',     // 创建时间
         updateTime: '',    // 更新时间
     };
-
-    /************************** 关联公司及集团管理end ************************************/
-
-    /***********************************用户管理相关state 开始************************************/
-
-    /** 当前页 */
-    currentPage: number;
-    /** 每页显示条数 */
-    pageSize: number;
-    /** 总条数 */
-    total: number;
-    /** 列表数组 */
-    data: any;
-    /** 详情 */
-    detail: Detail;
-    /** 筛选状态 */
-    searchState: boolean = false;
-    /** 筛选Icon样式 */
-    iconStyle: boolean = false;
-    /** 详情弹框状态 */
-    detailVisible: boolean = false;
-    /** 新增弹框状态 */
-    addVisible: boolean = false;
-    /** 编辑弹框状态 */
-    editVisible: boolean = false;
-    /** 角色弹框状态 */
-    roleVisible: boolean = false;
-    /** 用户所在公司角色数组 */
-    userCompanyRole: any;
-    /** 用户所在公司当前权限 */
-    userCurrentRole: any;
-    /** departmentIds */
-    departmentIds: any;
-    /** 用户列表数据加载状态 */
-    userDataLoding: boolean = true;
-
-    /***********************************用户管理相关state 结束************************************/
 
     /******************************** 角色 start ******************************************* */
     // 被选中的列表
@@ -124,28 +91,6 @@ interface ICompany {
     companyExpand?: boolean;
     activatedTab?: any;
     geturl?: any;
-}
-
-/**
- * 用户详情interface
- */
-interface Detail {
-    acceptStatus?: string;
-    companyId?: string;
-    credential?: string;
-    departmentId?: string;
-    departmentName?: string;
-    description?: string;
-    email?: string;
-    gUserId?: string;
-    isActivated?: boolean;
-    isDeleted?: boolean;
-    lastLoginIp?: string;
-    lastLoginTime?: number;
-    nickName?: string;
-    phone?: string;
-    userId?: string;
-    userType?: string;
 }
 
 interface IParamCondition {
